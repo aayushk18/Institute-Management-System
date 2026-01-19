0e182eab6a93fa05d77a5950251a251424289d80
aayushk18 <kumaraayush1807@gmail.com>
2026-01-19 15:13:36 +0530

    commit change

--- backend/index.js
+++ backend/index.js
@@ -42,12 +42,12 @@ app.use('/pics', express.static(path.join(path.resolve(), 'files', 'pics')));
 
 if (process.env.NODE_ENV == "production") {
     
-    app.use(express.static(path.join(__dirname,"../frontend/dist")))
+    app.use(express.static(path.join(__dirname, "../frontend/dist")));
 
-    app.get("/:path(*)"  ,(req,res)=>{
-
-        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"))
-    })
+    // CHANGE THIS LINE:
+    app.get("*", (req, res) => {
+        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+    });
 }
 
 
