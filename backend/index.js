482a17aca09a21be171ea6f58c81229383e07aee
aayushk18 <kumaraayush1807@gmail.com>
2026-01-19 15:49:17 +0530

    commit changes

--- backend/index.js
+++ backend/index.js
@@ -54,7 +54,7 @@ if (process.env.NODE_ENV == "production") {
 
     // CHANGE THIS LINE:
     app.get("/*", (req, res) => {
-        res.sendFile(path.join(__dirname, "../frontend", "dist", "index.html"));
+        res.sendFile(path.join(__dirname, "../frontend/dist/index.html"));
     });
 }
 
